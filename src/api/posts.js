8a1c9505f4c59c55afceb1e3ce1bd1430ab5a419
Prathiksha014 <prathikshavs03@gmail.com>
2024-02-25 13:35:47 +0530

    first commit

---
+++ src/api/posts.js
@@ -0,0 +1,104 @@
+import { BASE_URL } from "../config";
+
+
+
+const getPosts = async (token, query) => {
+  try {
+    const res = await fetch(
+      BASE_URL + "api/posts?" + new URLSearchParams(query),
+      {
+        headers: {
+          "x-access-token": token,
+        },
+      }
+    );
+    return await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getPost = async (postId, token) => {
+  try {
+    const res = await fetch(BASE_URL + "api/posts/" + postId, {
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    return await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+const createPost = async (post, user) => {
+  try {
+    const res = await fetch(BASE_URL + "api/posts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": user.token,
+      },
+      body: JSON.stringify(post),
+    });
+    return await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updatePost = async (postId, user, data) => {
+  try {
+    const res = await fetch(BASE_URL + "api/posts/" + postId, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "x-access-token": user.token,
+      },
+      body: JSON.stringify(data),
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deletePost = async (postId, user) => {
+  try {
+    const res = await fetch(BASE_URL + "api/posts/" + postId, {
+      method: "DELETE",
+      headers: {
+        "x-access-token": user.token,
+      },
+    });
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+  getPosts,
+  
+};
