5653668ce1cb00464ea9ad178a01febc8ad4decc
Prathiksha014 <prathikshavs03@gmail.com>
2024-02-25 16:35:08 +0530

    link changed

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
-let BASE_URL = "http://localhost:4000/";
+let BASE_URL = "https://social-media-application-backend.vercel.app/";
 
 export { BASE_URL };
